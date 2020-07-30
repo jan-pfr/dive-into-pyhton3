20d4050a4a838cd3ef5d7f32d6b938aa05ffcfb2
jan-pfr <jp@janpf.de>
2020-07-30 22:13:53 +0200

    input

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ surname = "Pfeiffer"
 age = 21
 notendurchschnitt = 2.3
 matrikuliert = True
+age = input("Ändere dein Alter: ")
+print("Dein Alter ist " + age)
+print("Dein Profil: " + name + ", " + surname + ", " + age)
