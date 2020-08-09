dd6663a50163952602401707455abce39f815ac5
Jan Pfeiffer <40457584@live.napier.ac.uk>
2020-08-09 18:27:01 +0200

    output changed.

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ surname = "Pfeiffer"
 age = 21
 notendurchschnitt = 2.3
 matrikuliert = True
+print("Dein Alter ist " + str(age))
 age = input("Ändere dein Alter: ")
-print("Dein Alter ist " + age)
-print("Dein Profil: " + name + ", " + surname + ", " + age)
+print("Dein Profil: " + name + ", " + surname + ", " + age + ", " + str(matrikuliert) + ", " + str(notendurchschnitt))
