9814a5f3ff1ba3a69f2280b44d024b21714ccbfc
jan-pfr <jp@janpf.de>
2020-08-10 21:47:32 +0200

    formatted string.

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-# name = "Jan"
-# surname = "Pfeiffer"
-# age = 21
-# notendurchschnitt = 2.3
-# matrikuliert = True
-# print("Dein Alter ist " + str(age))
-# age = input("Ändere dein Alter: ")
-# print("Dein Profil: " + name + ", " + surname + ", " + age + ", " + str(matrikuliert) + ", " + str(notendurchschnitt))
-#
+name = "Jan"
+surname = "Pfeiffer"
+age = 21
+notendurchschnitt = 2.3
+matrikuliert = True
+print("Dein Alter ist " + str(age))
+age = input("Ändere dein Alter: ")
+message = f"Dein Profil: {name}, {surname} ist {age} Jahre alt, und hat einen Notendurchschnitt von {notendurchschnitt}"
+print(message)
+
 geburtsjahr = input("Dein Geburtsjahr: ")
 print(type(geburtsjahr))
 age = 2020 - int(geburtsjahr)
