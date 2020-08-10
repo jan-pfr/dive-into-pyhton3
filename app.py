05078f7f65732333fa989849bbc9033fc47ac4d0
jan-pfr <jp@janpf.de>
2020-08-10 21:33:54 +0200

    Added a function that convert kg to lbs.

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-name = "Jan"
-surname = "Pfeiffer"
-age = 21
-notendurchschnitt = 2.3
-matrikuliert = True
-print("Dein Alter ist " + str(age))
-age = input("Ändere dein Alter: ")
-print("Dein Profil: " + name + ", " + surname + ", " + age + ", " + str(matrikuliert) + ", " + str(notendurchschnitt))
-
+# name = "Jan"
+# surname = "Pfeiffer"
+# age = 21
+# notendurchschnitt = 2.3
+# matrikuliert = True
+# print("Dein Alter ist " + str(age))
+# age = input("Ändere dein Alter: ")
+# print("Dein Profil: " + name + ", " + surname + ", " + age + ", " + str(matrikuliert) + ", " + str(notendurchschnitt))
+#
 geburtsjahr = input("Dein Geburtsjahr: ")
 print(type(geburtsjahr))
 age = 2020 - int(geburtsjahr)
 print(type(age))
 print(age)
+
+wight_kg = input("Ihr Gewicht in Kilo: ")
+wight_lbs = float(wight_kg) * 2.204
+print(wight_lbs)
