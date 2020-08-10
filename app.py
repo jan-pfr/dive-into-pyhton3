822a73a10eb5f0bdbb1033fc626a15172c28b484
jan-pfr <jp@janpf.de>
2020-08-10 21:19:12 +0200

    Added a function that calculates your age.

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ matrikuliert = True
 print("Dein Alter ist " + str(age))
 age = input("Ändere dein Alter: ")
 print("Dein Profil: " + name + ", " + surname + ", " + age + ", " + str(matrikuliert) + ", " + str(notendurchschnitt))
+
+geburtsjahr = input("Dein Geburtsjahr: ")
+print(type(geburtsjahr))
+age = 2020 - int(geburtsjahr)
+print(type(age))
+print(age)
