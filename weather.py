4b5908a2b1b20fc43e743a7cda6cf7d709533346
jan-pfr <jp@janpf.de>
2020-08-11 22:12:16 +0200

    if - else

---
+++ weather.py
@@ -0,0 +1,11 @@
+is_hot = True
+is_cold = True
+
+if is_hot:
+    print("Warmer Tag heute")
+    print("Trink Wasser")
+elif is_cold:
+    print("Kalter Tag heute")
+    print("Zieh dich warm an")
+else:
+    print("Schönes Wetter heute")
