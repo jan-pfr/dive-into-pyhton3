3bcdcaa7263ace1abd96a7af2b0e9bdd3a661562
jan-pfr <jp@janpf.de>
2020-08-11 21:56:37 +0200

    math operations

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import math # importing modules
 name = "Jan"
 surname = "Pfeiffer"
 age = 21
@@ -5,8 +6,11 @@ notendurchschnitt = 2.3
 matrikuliert = True
 print("Dein Alter ist " + str(age))
 age = input("Ändere dein Alter: ")
+# string formatting
 message = f"Dein Profil: {name}, {surname} ist {age} Jahre alt, und hat einen Notendurchschnitt von {notendurchschnitt}"
 print(message)
+# string operations
+
 print("Länge: " + str(len(message)))
 print(message.upper())
 print(message.lower())
@@ -24,3 +28,18 @@ print(age)
 wight_kg = input("Ihr Gewicht in Kilo: ")
 wight_lbs = float(wight_kg) * 2.204
 print(wight_lbs)
+# arithemtic operations
+
+x = 10 + 3 * 2 ** 2
+print(x) # should be 22
+y= 10
+y -=1
+print(y) # should be 9
+
+# math functions
+x = 20.4
+print(round(x))
+print(abs(-3.5)) # absolut number
+print(math.floor(3.1))
+
+
