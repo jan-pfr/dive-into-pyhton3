52bd8994ad2f3bf220207cb7effd1bd73639c0f3
jan-pfr <jp@janpf.de>
2020-11-29 17:07:41 +0100

    sbA08.py full functionality added. round() is optional.

--- sbA08.py
+++ sbA08.py
@@ -2,24 +2,20 @@ class Angestellter:
     Name = None
     Wochenstunden = None
     Jahresgehalt = None
-
-
     def __init__(self, Name, Wochenstunden, Jahresgehalt):
         self.Name = Name
         self.Wochenstunden = Wochenstunden
         self.Jahresgehalt = Jahresgehalt
         print(f'{self.Name} wurde eingestellt; Gehalt: {self.Jahresgehalt}€; Arbeitszeit: {self.Wochenstunden} Stunden')
-
     def __str__(self):
         return f'Angestellter {self.Name}s Gehalt beträgt {self.Jahresgehalt}€.'
-
     def Gehaltsanpassung(self, addValue):
         self.Jahresgehalt = self.Jahresgehalt + addValue
         self.Jahresgehalt = round(self.Jahresgehalt, 2)
 
 Mitarb1 = Angestellter('Hans Wurst', 35, 50000)
-
 i = 0
+
 while Mitarb1.Jahresgehalt <= 70000:
      rise = Mitarb1.Jahresgehalt * 0.03
      Mitarb1.Gehaltsanpassung(rise)
